f9c9437e71b55d98ced35135d2ca952a8f9dbdbe
DELL@Alsaeed <alsaeedalsafer@gmail.com>
2025-01-22 20:57:13 +0200

    saeed

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "I love Programming!" << endl << endl;
+    cout << "I Promise to be the best devolper ever!" << endl << endl;
+    cout << "I know it will take sometime to practice put i will achieve my goal." << endl << endl;
+    cout << "Best Regards!" << endl;
+    cout << "Alsaeed Abubaker." << endl << endl;
+
+    return 0;
+}
